00cbf9b847535ec831c756dc7abb11b33b263183
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-02-07 09:41:01 +0700

    update

--- assets/Script/C/CharacterController.ts
+++ assets/Script/C/CharacterController.ts
@@ -1,4 +1,4 @@
-import { _decorator, animation, Component, Node } from 'cc';
+import { _decorator, animation, Component, Node, ValueType } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('CharacterController')
@@ -6,12 +6,17 @@ export class CharacterController extends Component {
     @property(animation.AnimationController)
     private animationController:animation.AnimationController;
     start(){
-        this.jump();
+       
     }
     public jump() {
-        
         this.animationController.setValue('Jump',true);
     }
-}
+    public run(){
+        let ValueType = 0;
+        this.animationController.setValue('speed',ValueType);
+    }
+    
+   
+    }
 
 
